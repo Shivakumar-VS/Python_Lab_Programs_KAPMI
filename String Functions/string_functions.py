9410018e910d4c46219cc1ec41ed138a2d600c2b
Shivakumar-001 <shivagana2004@gmail.com>
2025-04-08 21:32:37 +0530

    Program to explore string functions

---
+++ String Functions/string_functions.py
@@ -0,0 +1,19 @@
+text = "welcome to Kapmc"
+
+print("\nUpperacase String:")
+print(text.upper())
+
+print("\nLowercase String:")
+print(text.lower())
+
+print("\nTitlecase String:")
+print(text.title())
+
+print("\nCapitalize String:")
+print(text.capitalize())
+
+print("\nSwapcase String:")
+print(text.swapcase())
+
+print("orirginal string:")
+print(text)
